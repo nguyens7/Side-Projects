4685490c33ca6b7dd524d73874011afd646bd553
nguyens7 <nguyens7@msu.edu>
2017-10-22 19:17:30 -0400

    Added PCA and Wisc cancer dataset

--- Kmeans_clustering.R
+++ Kmeans_clustering.R
@@ -1,6 +1,9 @@
 library(tidyverse)
 library(factoextra)
 library(babynames)
+library(ggfortify)
+library(cowplot)
+library(ggrepel)
 
 # Babynames ---------------------------------------------------------------
 babynames <- babynames
@@ -40,4 +43,70 @@ fviz_cluster(km_females, data = Females,
              ggtheme = theme_minimal(),
              main = "Clustering of Top 5 Female Names\n From 1880-2015")
 
+# Compare Jennifer to Mary
+interesting <- c("Jennifer", "Mary","Helen","Sophia")
 
+babynames %>%
+  filter(sex == "F" &
+         name %in% interesting ) %>% 
+  ggplot(aes(x = year, y = n, color = name)) +
+  geom_line()
+
+
+names(km_females$cluster[km_females$cluster==8])
+
+
+# Wisconsin Cancer Study --------------------------------------------------
+
+data <- "https://assets.datacamp.com/production/course_1815/datasets/WisconsinCancer.csv"
+cancer <- read_csv(data)
+
+cancer_matrix <- cancer %>% 
+  select(-X33, -diagnosis, -contains("_se")) %>% 
+  remove_rownames() %>% 
+  column_to_rownames(var = "id") %>% 
+  as.matrix()
+
+cancer_PCA <- prcomp(cancer_matrix, scale = TRUE)
+
+cancer_PCA_df <- as.data.frame(cancer_PCA$rotation)
+cancer_PCA_df$id <- row.names(cancer_PCA_df)
+
+cancer_PCA_df
+
+cancer_PCA_df %>% 
+  ggplot(aes(x=PC1, y=PC2, label=id, color=id )) +
+  geom_point() +
+  geom_text_repel(size = 6)
+
+
+fviz_eig(cancer_PCA)
+
+fviz_pca_ind(cancer_PCA,
+             col.ind = "contrib", # Color by the quality of representation,
+             geom.ind = c("point"),  # "text"
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = FALSE )     # Avoid text overlapping
+
+fviz_pca_var(cancer_PCA,
+             col.var = "contrib", # Color by contributions to the PC
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE)     # Avoid text overlapping
+
+
+fviz_pca_biplot(cancer_PCA, repel = TRUE,
+                col.var = "#2E9FDF", # Variables color
+                col.ind = "#696969")  # Individuals color
+
+
+autoplot(scale(cancer_matrix))
+
+cancer_test <-  cancer %>%
+   select(-X33) %>% 
+    mutate_at(vars(diagnosis),as.factor)
+ 
+
+#  USArrests --------------------------------------------------------------
+
+USArrests
+ 
